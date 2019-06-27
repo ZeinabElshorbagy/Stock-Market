63c6915cccdb1fde96c28e75195702a893aab85f
ZeinabElshorbagy <zeinabelshorbagy@gmail.com>
2019-06-27 23:25:38 +0200

    valites uniqueness of asset name

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,6 @@
 class Asset < ApplicationRecord
   validates_presence_of :name, :price
-  validates :name, length: {minimum: 2}
+  validates :name, uniqueness: {case_sensitive: false}, length: {minimum: 2}
+  validates_uniqueness_of :name
   validates_numericality_of :price, greater_than_or_equal_to: 0, numericality: { only_float: true }
 end
