d15ad3fe025c5f00219d5d26337a0a1da30a68b9
ZeinabElshorbagy <zeinabelshorbagy@gmail.com>
2019-06-28 00:11:33 +0200

    add and update features done

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,5 +1,48 @@
 class AssetsController < ApplicationController
+  #before_action :authenticate_user!, except: [:index, :show]
+  #before_action :authenticate_admin?, except: [:index, :show]
+
   def index
     render json: {assets: Asset.all}
+  end
+
+  def show
+    render json: asset
+  end
+
+  def create
+    @asset = Asset.create(name: asset_params['name'], price: asset_params['price'])
+    if @asset.save!
+      render json: {asset: @asset}, status: :created
+    else
+      render json: {errors: @asset.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if asset.update_price! asset_price_params['price']
+      render json: {msg: :updated, asset: asset}, status: :created
+    else
+      render json: {errors: @asset.errors}, status: :unprocessable_entity
+    end
+  end
+
+
+
+  private
+
+  def asset
+    @asset || Asset.find(asset_id_params['id'])
+  end
+
+  def asset_id_params
+    params.permit(:id)
+  end
+
+  def asset_params
+    params.require(:asset).permit(:name, :price)
+  end
+  def asset_price_params
+    params.require(:asset).permit(:price)
   end
 end
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -3,4 +3,13 @@ class Asset < ApplicationRecord
   validates :name, uniqueness: {case_sensitive: false}, length: {minimum: 2}
   validates_uniqueness_of :name
   validates_numericality_of :price, greater_than_or_equal_to: 0, numericality: { only_float: true }
+
+  def update_price!(price)
+    if price.present?
+      self.price = price
+      self.save!
+    else
+      erros.add(:price, 'price should exists!!!!')
+    end
+  end
 end
